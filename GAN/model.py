ead8853557e4697b307d216d5313277658fe6841
cc <412214410@qq.com>
2017-07-03 18:17:43 +0800

    update gan git

--- GAN/model.py
+++ GAN/model.py
@@ -69,8 +69,13 @@ class SampleNNGan(object):
         g_params = [w1, b1, w2, b2, w3, b3]
         return x_generate, g_params
 
-    def discriminator(self):
-        pass
+    def discriminator(self,x_data, x_generated, keep_prob):
+
+        x_input = tf.concat([x_data,x_generated],axis=0)
+        w1 = tf.Variable(tf.truncated_normal([self.img_size,self.hidden_layer_2],stddev=0.1), name="d_w1", dtype=tf.float32)
+        b1 = tf.Variable(tf.zeros([self.hidden_layer_2]), name="d_b1", dtype=tf.float32)
+        h1 = tf.nn.dropout(tf.nn.relu(tf.matmul(x_input, w1)+b1), keep_prob)
+        w2 = tf.Variable(tf.truncated_normal([self.hidden_layer_2,self.hidden_layer_1],stddev=0.1), name="d_w2", dtype=tf.float32)
 
     def build_graph(self):
         pass
