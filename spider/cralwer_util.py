49645bda2e73643eb4e5ddac77103584affa19c2
cc <412214410@qq.com>
2017-10-24 19:02:10 +0800

    shooter spider update

--- spider/cralwer_util.py
+++ spider/cralwer_util.py
@@ -9,9 +9,13 @@
 @ time: $17-10-20 上午11:50
 """
 
+import sys
+import os
 import random
 import requests
 from pprint import pprint
+import threading
+import datetime
 
 
 USER_AGENTS = [
@@ -73,3 +77,55 @@ class BaseCrawler(object):
         if raw:
             return raw.content
         return None
+
+
+def Handler(start, end, url, filename):
+    headers= {'Range': 'bytes=%d-%d'%(start, end)}
+    res = requests.get(url, headers=headers, stream=True)
+    with open(filename, 'r+b') as f:
+        # 移动到起始位置
+        f.seek(start)
+        var = f.tell()
+        f.write(res.content)
+
+
+def download_multi_thread(url, file_path, num_thread=5):
+    res = requests.head(url)
+    try:
+        file_name = res.headers['Content-Disposition']
+        # rewrite part for different url
+        # this for shooter
+        file_name = file_name.split(';')[-1].split('="')[-1][:-1]
+        file_size = int(res.headers['Content-Length'])
+
+    except Exception, e:
+        print 'get url file failed for: %s'%str(e)
+        return 0
+    file_name = os.path.join(file_path, file_name)
+    # 创建一个和要下载文件一样大小的文件
+    fp = open(file_name, "wb")
+    fp.truncate(file_size)
+    fp.close()
+
+    # 启动多线程写文件
+    part = file_size // num_thread  # 如果不能整除，最后一块应该多几个字节
+    for i in range(num_thread):
+        start = part * i
+        if i == num_thread - 1:  # 最后一块
+            end = file_size
+        else:
+            end = start + part
+
+        t = threading.Thread(target=Handler, kwargs={'start': start, 'end': end, 'url': url, 'filename': file_name})
+        t.setDaemon(True)
+        t.start()
+    # 等待所有线程下载完成
+    main_thread = threading.current_thread()
+    for t in threading.enumerate():
+        if t is main_thread:
+            continue
+        t.join()
+    print('%s 下载完成' % file_name)
+    return 1
+
+# download_multi_thread('http://shooter.zimuku.net/download/NTcwNjV8Z2xhZGlhdG9yXzIwMDBfZXh0ZW5kZWRfc3Vic19yMy5yYXI%3D')
--- spider/crawler_zimu.py
+++ spider/crawler_zimu.py
@@ -12,8 +12,9 @@
 from bs4 import BeautifulSoup
 import requests
 from logger import logger
-from cralwer_util import BaseCrawler
-
+from cralwer_util import BaseCrawler, download_multi_thread
+import requests
+import time
 
 # logger = logger.logger
 SAVING_PATH = '/home/showlove/cc/shooter'
@@ -25,28 +26,49 @@ class ShooterCrawler(BaseCrawler):
     def __init__(self):
         super(ShooterCrawler, self).__init__()
 
-    def save(self, down_load_link):
-        pass
+    def save(self, down_load_link, saving_path=SAVING_PATH):
+        if download_multi_thread(down_load_link, saving_path):
+            logger.info('success download file')
+        else:
+            logger.error('Fun [download_multi_thread] failed')
 
     def get_down_load_url(self, link_url):
-        return ''
+        try:
+            download_content = self.get(link_url)
+            download_soup = BeautifulSoup(download_content)
+            download_a = download_soup.find('a', attrs={'id': 'down1'})
+            download_link = download_a.attrs['href']
+            # download_url = BASE_URL + download_link
+            if 'http' in download_link:
+                return download_link
+            else:
+                return BASE_URL + download_link
+        except Exception, e:
+            logger.error('[get_down_load_url] failed for: %s'%str(e))
+            return None
 
     def run(self):
         try:
-            for page_num in range(1,12994):
-                logger.info()
+            # for page_num in range(1,12994):
+            for page_num in range(195, 12994):
+                logger.info('searching shooter in page %d'%page_num)
                 url = SEARCHING_URL.format(page_num)
                 content = self.get(url)
                 soup = BeautifulSoup(content)
-                div_list = soup.find_all('div',attrs={'class':'persub clearfix'})
+                div_list = soup.find_all('div', attrs={'class': 'persub clearfix'})
                 for index, div_item in enumerate(div_list):
-                    _link = div_item.find('h1').find('a').attrs('href')
+                    logger.info('page %d ---- downloading No.%d file....'%(page_num, index))
+                    _link = div_item.find('h1').find('a').attrs['href']
                     link = BASE_URL + _link
                     down_load_link = self.get_down_load_url(link)
-                    self.save(down_load_link)
+                    if down_load_link:
+                        self.save(down_load_link)
+                    else:
+                        logger.warn('failed get download url in page %s'%link)
+
+                    time.sleep(5)
         except Exception, e:
             logger.error('searching download page failed for : %s'%str(e))
 
 
-    def save(self, saving_path=SAVING_PATH):
-        pass
+crawler = ShooterCrawler().run()
