84d9960135418573f307b9da9c94a8fac9e3bb10
invorker4zoo <412214410@qq.com>
2018-04-10 17:58:34 +0800

    begin style trans project

---
+++ style_trans/neural_style.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+@ license: Apache Licence
+@ github: invoker4zoo
+@ author: invoker/cc
+@ wechat: whatshowlove
+@ software: PyCharm
+@ file: neural_style
+@ time: 18-4-10
+"""
+
+import tensorflow as tf
